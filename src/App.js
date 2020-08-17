9e1ee3f19f2e8f0ae63c61b5fee56a7cf9c77a60
Jordão Victor <jordaovpinheiro@gmail.com>
2020-08-17 03:27:40 -0300

    Accordion done

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import './GlobalStyles.scss'
 import Banner from './components/Banner'
 import Carousel from './components/CarrouselListPage'
 import Profile from './components/Profile'
-
+import Accordion from './components/Accordion'
 function App() {
   return (
     <>
       <Banner/>
       <Carousel/>
       <Profile/>
+      <Accordion/>
     </>
   );
 }
---
+++ src/components/Accordion/index.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import './style.scss'
+
+import image from '../../assets/background.jpg'
+
+const Page = ()=> {
+    return(
+        <section className="accordion-page ">
+            <div className="accordion border">
+                <div className="toggle ">
+                    <input type="checkbox" name="user-accordion" id="section1" className="accordion-input "/>
+                    <label for="section1" className="accordion-label border-top">Lorem Ipsum</label>
+                    <div className="accordion-content">
+                        <div className="accordion-container">
+                            <img src={image} alt=""/>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="toggle">
+                    <input type="checkbox" name="user-accordion" id="section2" className="accordion-input"/>
+                    <label for="section2" className="accordion-label">Lorem Ipsum</label>
+                    <div className="accordion-content">
+                        <div className="accordion-container">
+                            <img src={image} alt=""/>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="toggle">
+                    <input type="checkbox" name="user-accordion" id="section3" className="accordion-input"/>
+                    <label for="section3" className="accordion-label">Lorem Ipsum</label>
+                    <div className="accordion-content">
+                        <div className="accordion-container">
+                            <img src={image} alt=""/>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="toggle">
+                    <input type="checkbox" name="user-accordion" id="section4" className="accordion-input"/>
+                    <label for="section4" className="accordion-label">Lorem Ipsum</label>
+                    <div className="accordion-content">
+                        <div className="accordion-container">
+                            <img src={image} alt=""/>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="toggle">
+                    <input type="checkbox" name="user-accordion" id="section5" className="accordion-input"/>
+                    <label for="section5" className="accordion-label ">Lorem Ipsum</label>
+                    <div className="accordion-content border-bottom">
+                        <div className="accordion-container">
+                            <img src={image} alt=""/>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Page;
